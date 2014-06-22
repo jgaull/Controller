90ab1ec437955e0090346c30c392b2dfc2f8f899
Jonathan Gaull <jonbeesh@gmail.com>
2014-06-21 17:44:35 -0700

    Added events for ride start and ride finished.

--- ModeoRide/DataProcessing.ino
+++ ModeoRide/DataProcessing.ino
@@ -4,6 +4,7 @@ void manageDataProcessing() {
     //handleStrainMessage(rxData[DAT_RID_TRQ]);
     handleStrainMessageLight(rxData[DAT_RID_TRQ]);
     rxDataIsFresh[DAT_RID_TRQ] = false;
+    handleRideStart();
   }
   
   if (rxDataIsFresh[DAT_MTR_TMP]) {
@@ -34,12 +35,29 @@ void manageDataProcessing() {
       
       mapSpeedToDamping(rxData[DAT_MTR_SPD]);
       modeo.setValueForSensor(mappedSpeed, SENSOR_SPEED);
-      
+      handleRideStart();
     }
     
     rxDataIsFresh[DAT_MTR_SPD] = false;
   }
+  
+  if (startRideTimestamp > 0) {
+    unsigned short duration = millis() - startRideTimestamp;
+    modeo.setUnsignedShortValueForProperty(duration, PROPERTY_RIDE_DURATION);
+  }
  
+}
+
+void handleRideStart() {
+  if (startRideTimestamp == 0 && filteredRiderEffort > 5 && rxData[DAT_MTR_SPD] > 4) {
+    Serial.println("Started Ride!");
+    startRideTimestamp = millis();
+    
+    byte event[1];
+    event[0] = EVENT_START_RIDE;
+    modeo.setValueForProperty(PROPERTY_EVENT, event);
+    modeo.setValueForSensor(1, SENSOR_HAS_EVENT);
+  }
 }
 
 //Bezier intersection functions
